7905289cc9b5b01408dada36ab2c4746f697bd4d
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-10-06 14:14:53 +0200

    fix(biome): update for changed doc locations

--- scripts/biome-docs-search.js
+++ scripts/biome-docs-search.js
@@ -13,11 +13,11 @@ function alfredMatcher(str) {
 //──────────────────────────────────────────────────────────────────────────────
 
 /** @type {AlfredRun} */
-// rome-ignore lint/correctness/noUnusedVariables: Alfred run
+// biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
 	const docsUrl = "https://api.github.com/repos/biomejs/biome/git/trees/main?recursive=1";
 	const baseUrl = "https://biomejs.dev";
-	const docPathRegex = /^website\/src\/pages\/(.*)\.mdx?$/i;
+	const docPathRegex = /^website\/src\/content\/docs\/(.*)\.mdx?$/i;
 
 	const workArray = JSON.parse(app.doShellScript(`curl -sL "${docsUrl}"`))
 		.tree.filter((/** @type {{ path: string; }} */ file) => docPathRegex.test(file.path))
