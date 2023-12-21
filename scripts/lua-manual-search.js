0bcd5eb6b53f9caea47d0660a47df05f341fe439
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2023-12-21 01:35:45 +0100

    refactor(lua): simplify structure

--- scripts/lua-manual-search.js
+++ scripts/lua-manual-search.js
@@ -8,8 +8,8 @@ app.includeStandardAdditions = true;
 /** @param {string} str */
 function alfredMatcher(str) {
 	const clean = str.replace(/[-()_.:#/\\;,[\]]/g, " ");
-	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
-	return [clean, camelCaseSeperated, str].join(" ");
+	const camelCaseSeparated = str.replace(/([A-Z])/g, " $1");
+	return [clean, camelCaseSeparated, str].join(" ");
 }
 
 //──────────────────────────────────────────────────────────────────────────────
@@ -17,24 +17,25 @@ function alfredMatcher(str) {
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
 	const luaVersion = $.getenv("lua_version");
-	const luaManualBaseURL = `https://www.lua.org/manual/${luaVersion}/`;
+	const luaManualBaseURL = `http://www.lua.org/manual/${luaVersion}/`;
 	const luaWikiBaseURL = "http://lua-users.org";
 
 	const ahrefRegex = /.*?href="(.*?)">(.*?)<.*/i;
-	const jsonArr = [];
 
 	const rawHTML =
 		app.doShellScript(`curl -sL '${luaManualBaseURL}'`) +
 		app.doShellScript(`curl -sL '${luaWikiBaseURL}/wiki/LuaDirectory'`);
 
-	rawHTML
+	const sites = rawHTML
 		.split("\r")
-		.filter((line) => line.toLowerCase().includes("href") && !line.includes("css") && !line.includes("IMG"))
-		.forEach((line) => {
+		.filter(
+			(line) => line.toLowerCase().includes("href") && !line.includes("css") && !line.includes("IMG"),
+		)
+		.map((line) => {
 			const subsite = line.replace(ahrefRegex, "$1");
 			const isWiki = subsite.includes("wiki");
 			let title = line.replace(ahrefRegex, "$2").replaceAll("&ndash; ", "");
-			if (title.includes(">")) return;
+			if (title.includes(">")) return {};
 
 			let type = "manual";
 			if (isWiki) type = "wiki";
@@ -45,14 +46,14 @@ function run() {
 			let url = isWiki ? luaWikiBaseURL : luaManualBaseURL;
 			url += subsite;
 
-			jsonArr.push({
+			return {
 				title: title,
 				subtitle: type,
 				match: alfredMatcher(title),
 				arg: url,
 				uid: url,
-			});
+			};
 		});
 
-	return JSON.stringify({ items: jsonArr });
+	return JSON.stringify({ items: sites });
 }
