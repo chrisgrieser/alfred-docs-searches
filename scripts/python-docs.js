17875c229f6675a69bae887bb456d04f7467e514
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-03-06 17:45:59 +0100

    fix: python docs search

--- scripts/python-docs.js
+++ scripts/python-docs.js
@@ -22,18 +22,13 @@ function readFile(path) {
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	// INFO HOW TO UPDATE SEARCH INDEX
-	// 1. Download it from https://docs.python.org/3/download.html
-	// 2. Frm the search, fetch the file `searchindex.js` and put it under the
-	// name below into this workflows `data` folder
-	// 3. Append the python version for documentation purposes
-	// TODO see how to automate this later
-	const searchIndexFile = "./data/searchindex_python-3.12.2.js";
+	// INFO 
+	// see the file `.data/HOWTO_update-python-searchindex` on how to update the index
+	const searchIndexFile = "./data/searchindex_python-3.12.2.json";
 
 	const pythonVersion = searchIndexFile.match(/\d\.\d+/)[0];
 	const baseUrl = `https://docs.python.org/${pythonVersion}/`;
-	// `slice` to remove the `Search.setIndex(…)` enclosing the json we want
-	const searchIndex = JSON.parse(readFile(searchIndexFile).slice(16, -1));
+	const searchIndex = JSON.parse(readFile(searchIndexFile));
 
 	const sites = [];
 	for (const [entryTitle, entry] of Object.entries(searchIndex.indexentries)) {
@@ -58,6 +53,6 @@ function run() {
 	return JSON.stringify({
 		items: sites,
 		// 1 week (using our offline index, so it's always up to date)
-		// cache: { seconds: 3600 * 24 * 7 },
+		cache: { seconds: 3600 * 24 * 7 },
 	});
 }
