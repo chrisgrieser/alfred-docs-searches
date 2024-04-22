3ac255a9ae1247697af4d7449213b9c16330f7fe
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-04-22 10:16:47 +0200

    chore: leftpad keyword numbers & cleanup code

--- devdocs/update-available-devdocs.mjs
+++ devdocs/update-available-devdocs.mjs
@@ -10,12 +10,8 @@ const shortHands = {
 	hammerspoon: "hs",
 };
 
-// to be run from repo root
-const paths = {
-	infoPlist: "./info.plist",
-	keywordSlugMap: "./.github/keyword-slug-map.json",
-};
-
+//──────────────────────────────────────────────────────────────────────────────
+// INFO to be run from repo root
 //──────────────────────────────────────────────────────────────────────────────
 
 async function run() {
@@ -39,28 +35,32 @@ async function run() {
 		infoPlistPopup.push(line);
 	}
 
-	fs.writeFileSync(paths.keywordSlugMap, JSON.stringify(allLangs));
+	// create keyword-slug-map
+	if (!fs.existsSync("./.github/")) fs.mkdirSync("./.github/");
+	fs.writeFileSync("./.github/keyword-slug-map.json", JSON.stringify(allLangs));
 
 	// update `info.plist` to insert all languages as options
 	/** @type {string[]} */
-	const xmlLines = fs.readFileSync(paths.infoPlist, "utf8").split("\n");
+	const xmlLines = fs.readFileSync("./info.plist", "utf8").split("\n");
 
 	// create multiple popups to select in Alfred config
-	const popupNumber = 20;
+	const numberOfPopups = 20;
 	let newXmlLines = [];
-	for (let i = 1; i <= popupNumber; i++) {
+	for (let i = 1; i <= numberOfPopups; i++) {
 		const label = i === 1 ? "Enabled devdocs" : "";
+		const number = i.toString().padStart(2, "0");
+
 		newXmlLines = newXmlLines.concat([
 			"<dict> <key>config</key> <dict> <key>default</key> <string></string> <key>pairs</key> <array>",
 			...infoPlistPopup,
-			`</array> </dict> <key>description</key> <string></string> <key>label</key> <string>${label}</string> <key>type</key> <string>popupbutton</string> <key>variable</key> <string>keyword_${i}</string> </dict>`,
+			`</array> </dict> <key>description</key> <string></string> <key>label</key> <string>${label}</string> <key>type</key> <string>popupbutton</string> <key>variable</key> <string>keyword_${number}</string> </dict>`,
 		]);
 	}
 
 	const start = xmlLines.indexOf("\t<key>userconfigurationconfig</key>") + 2;
 	const end = xmlLines.indexOf("\t</array>", start);
 	xmlLines.splice(start, end - start, ...newXmlLines);
-	fs.writeFileSync(paths.infoPlist, xmlLines.join("\n"));
+	fs.writeFileSync("./info.plist", xmlLines.join("\n"));
 }
 
 await run();
