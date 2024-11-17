1a717a232c58b75a2981422c6967e66447cd221b
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-11-17 11:58:29 +0100

    chore: minor cleanup

--- scripts/devdocs/search-devdocs.js
+++ scripts/devdocs/search-devdocs.js
@@ -108,8 +108,7 @@ function run() {
 		});
 	const replacement = pinnedVersions.find((version) => version.used === keyword);
 	if (replacement) {
-		// biome-ignore lint/suspicious/noConsole: <explanation>
-		// biome-ignore lint/suspicious/noConsoleLog: <explanation>
+		// biome-ignore lint/suspicious/noConsole: intentional
 		console.log("Pinned version found.");
 		languageSlug = replacement.pinned;
 	}
@@ -125,7 +124,7 @@ function run() {
 		const iconExists = fileExists(iconpath);
 
 		const indexUrl = `https://documents.devdocs.io/${languageSlug}/index.json`;
-		// biome-ignore lint/suspicious/noConsole: <explanation>
+		// biome-ignore lint/suspicious/noConsole: intentional
 		console.log("indexUrl:", indexUrl);
 
 		/** @type {DevDocsIndex} */
